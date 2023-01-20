1736f39cf6c059b9631cc498afc154fe26928263
Meng-Yun Wang <mengyun.wang@uib.no>
2023-01-20 19:37:46 +0800

    Update 1.fmriprep.sh

--- fMRI/Prep/fMRIprep/1.fmriprep.sh
+++ fMRI/Prep/fMRIprep/1.fmriprep.sh
@@ -1,19 +1,5 @@
 #bash
 
-# crash crash crash, maybe related with RAM 
-docker run -ti --rm \
-    -v /Users/wang/Desktop/Research_projects/BBSC/Functional/Data/Reorganized:/data:ro \
-    -v /Users/wang/Desktop/Research_projects/BBSC/Functional/fMRI_Prep:/out \
-    -v /Users/wang/Desktop/Freesurfer/license.txt:/opt/freesurfer/license.txt \
-    nipreps/fmriprep:22.0.1 \
-    /data /out \
-    participant \
-    --skip_bids_validation --participant_label 1 --cifti-output 91k --medial-surface-nan \
-    --use-aroma --error-on-aroma-warnings \
-    --fs-subjects-dir /Users/wang/Desktop/Research_projects/BBSC/Functional/T1_analysis \
-    --output-spaces MNI152NLin6Asym:res-2 MNI152NLin2009cAsym \
-    --output-layout bids --low-mem --resource-monitor --verbose
-
 # analysis anatomical data only
 docker run -ti --rm \
     -v /Users/wang/Desktop/Research_projects/BBSC/Functional/Data/Reorganized:/data:ro \
@@ -23,7 +9,7 @@ docker run -ti --rm \
     /data /out participant \
     --skip_bids_validation --participant_label 1 --anat-only \
     --fs-subjects-dir /Users/wang/Desktop/Research_projects/BBSC/Functional/T1_analysis \
-    --output-spaces MNI152NLin6Asym:res-2 MNI152NLin2009cAsym \ #can only use one
+    --output-spaces MNI152NLin6Asym:res-2 \ 
     --output-layout bids --resource-monitor --verbose
 
 # preprocessing the BOLD data only
@@ -35,18 +21,11 @@ docker run -ti --rm \
     /data /out participant \
     --skip_bids_validation --participant_label 1 --fs-no-reconall \
     --fs-subjects-dir /Users/wang/Desktop/Research_projects/BBSC/Functional/T1_analysis \
-    --output-spaces MNI152NLin6Asym:res-2 MNI152NLin2009cAsym \ #can only use one
+    --output-spaces MNI152NLin6Asym:res-2 \
     --output-layout bids --resource-monitor --verbose  
-
-
-# project BOLD to surface template?
-docker run -ti --rm \
-    -v /Users/wang/Desktop/Research_projects/BBSC/Functional/Data/Reorganized:/data:ro \
-    -v /Users/wang/Desktop/Research_projects/BBSC/Functional/fMRI_Prep:/out \
-    -v /Users/wang/Desktop/Freesurfer/license.txt:/opt/freesurfer/license.txt \
-    nipreps/fmriprep:22.0.1 \
-    /data /out participant \
-    --skip_bids_validation --participant_label 1 --cifti-output 91k --medial-surface-nan \
-    --fs-subjects-dir /Users/wang/Desktop/Research_projects/BBSC/Functional/T1_analysis/sub-1 \
-    --output-spaces MNI152NLin6Asym:res-2\
-    --output-layout bids --nprocs 6 --resource-monitor --verbose
+    
+    
+ # suggestions to use fmriprep:
+ # 1. maybe crash because the RAM is small, so limit the process to 4gb per core (using 4 cores even you have 8 cores if the RAM is only 16G)
+ # 2. do not be greedy, means if you want all things from fMRIprep (cifiti format, ica, ...), it probabaly crash unless you have a powerful computer;
+ #.   so, optimize what you want
