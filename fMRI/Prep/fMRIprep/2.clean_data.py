67cc06ead991f7185c77185ed436dc899f658c1d
Meng-Yun Wang <mengyun.wang@uib.no>
2023-07-12 23:16:28 +0200

    Add files via upload

--- fMRI/Prep/fMRIprep/2.clean_data.py
+++ fMRI/Prep/fMRIprep/2.clean_data.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-clean the data preprocessed from frmriprep with nilearn
+clean the data preprocessed from frmriprep
 
 By M.-Y. Wang 23-Nov-2022
 """
@@ -15,9 +15,10 @@ import nibabel as nib
 import bids
 import pandas as pd
 
-fmriprep_dir ='/Users/wang/Desktop/Research_projects/BBSC/Functional/fMRI_Prep'
+fmriprep_dir ='/Users/wang/Desktop/Research_projects/BBSC/Functional/Prep/fMRIprep'
+data_save_dir = '/Users/wang/Desktop/Research_projects/BBSC/Functional/Parcellation/data/cleaned_data/'
 os.chdir(fmriprep_dir)
-
+os.mkdir(data_save_dir)
 #------- extract the preprocessed data from fmriPrep
 layout = bids.BIDSLayout(fmriprep_dir,validate=False,
                         config=['bids','derivatives'])
@@ -27,14 +28,14 @@ sub_list.sort()
 
 #--- first loop, pull out all the funcitonal data, mask data, and confounds from subjects
 for subi,sub_name in enumerate(sub_list):
-    func_files = layout.get(subject=sub_list[subi],
+    func_files = layout.get(subject=sub_name,
                         datatype='func', task='rest',
                         desc='preproc',
                         space='MNI152NLin6Asym',
                         extension='nii.gz',
                        return_type='file')
 
-    mask_files = layout.get(subject=sub_list[subi],
+    mask_files = layout.get(subject=sub_name,
                         datatype='func', task='rest',
                         desc='brain',
                         suffix='mask',
@@ -42,7 +43,7 @@ for subi,sub_name in enumerate(sub_list):
                         extension="nii.gz",
                        return_type='file')
 
-    confound_files = layout.get(subject=sub_list[subi],
+    confound_files = layout.get(subject=sub_name,
                             datatype='func', task='rest',
                             desc='confounds',
                            extension="tsv",
@@ -73,20 +74,20 @@ for subi,sub_name in enumerate(sub_list):
 
          #First we'll load in our data and check the shape
         raw_func_img = nimg.load_img(func_file)
-        raw_func_img.shape
+        #raw_func_img.shape
 
         func_img = raw_func_img.slicer[:,:,:,4:]
-        func_img.shape
+        #func_img.shape
 
          #Drop confound dummy TRs
         drop_confound_df = confound_df.loc[4:]
          #print(drop_confound_df.shape) #number of rows should match that of the functional image
-        drop_confound_df.head()
+        #drop_confound_df.head()
 
         confounds_matrix = drop_confound_df.values
 
          #Confirm matrix size is correct
-        confounds_matrix.shape
+        #confounds_matrix.shape
 
          #Set some constants
         high_pass= 0.009
@@ -98,8 +99,8 @@ for subi,sub_name in enumerate(sub_list):
                          low_pass=low_pass,high_pass=high_pass,t_r=t_r, mask_img=mask_file)
                          
 
-        clean_img.to_filename('./sub-%s/ses-%s/func/sub-%s_ses-%s_task-rest_space-MNI152NLin6Asym_res-2_desc-preproc_bold_clean_normalized.nii.gz'\
-             % (str(subi+1).zfill(2),str(sesi+1).zfill(2),str(subi+1),str(sesi+1)))
+        clean_img.to_filename(data_save_dir + 'sub-%s/sub-%s_ses-%s_task-rest_space-MNI152NLin6Asym_res-2_desc-preproc_bold_clean_normalized.nii.gz'\
+             % (str(subi+1).zfill(2),str(subi+1).zfill(2),str(sesi+1).zfill(2)))
 
 
 #Let's visualize our result!
